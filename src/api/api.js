e97398a764832edeb5224a467a44dc2788ea000b
Pavlo Pihula <rogertaylor1949@gmail.com>
2018-11-23 08:55:30 -0500

    building ui (bootstrap)

--- src/api/api.js
+++ src/api/api.js
@@ -26,6 +26,22 @@ class Weather {
     return await response.json();
   }
 
+  // Get forecast
+  async getForecastByCity() {
+    const init = await fetch(
+        `${this.base}data/2.5/forecast?q=${this.city}&APPID=${this.apiKey}`
+      ),
+      data = await init.json();
+
+    // Convert city into ID to more precise (accoring to API docs: https://openweathermap.org/current)
+    const response = await fetch(
+      `${this.base}data/2.5/forecast?id=${data.city.id}&units=${
+        this.unit
+      }&APPID=${this.apiKey}`
+    );
+    return await response.json();
+  }
+
   // Get current weather by coordinates
   getCoordinates() {
     return new Promise((res, rej) => {
@@ -45,41 +61,12 @@ class Weather {
     });
   }
 
-  // Get forecast
-  async getForecastByCity() {
-    const init = await fetch(
-        `${this.base}data/2.5/forecast?q=${this.city}&APPID=${this.apiKey}`
-      ),
-      data = await init.json();
-
-    // Convert city into ID to more precise (accoring to API docs: https://openweathermap.org/current)
-    const response = await fetch(
-      `${this.base}data/2.5/forecast?id=${data.city.id}&units=${
-        this.unit
-      }&APPID=${this.apiKey}`
-    );
-    return await response.json();
-  }
-
-  // Get forecast by coordinates
-  async getForecastByCoordinates() {
-    return this.getCoordinates().then(async data => {
-      const response = await fetch(
-        `${this.base}data/2.5/forecast?lat=${data.coords.latitude}&lon=${
-          data.coords.longitude
-        }&units=${this.unit}&APPID=${this.apiKey}
-      `
-      );
-      return await response.json();
-    });
-  }
-
   // Change location
   changeLocation(city) {
     this.city = city;
   }
 
-  // Change location
+  // Change unit
   changeUnit(unit) {
     this.unit = unit;
   }
--- src/app.js
+++ src/app.js
@@ -1,15 +1,75 @@
+/**
+ * Main APP js
+ *
+ */
 import { api } from "./api/api";
-import { throws } from "assert";
 
-document.getElementById("citySearch").addEventListener("submit", e => {
-  const city = document.getElementById("mainSearch").value;
-  api.changeLocation(city);
-  api.getCurrentByID().then(data => console.log(data));
-  e.preventDefault();
-});
-
+document.addEventListener("DOMContentLoaded", loadWeather);
+document
+  .getElementById("citySearch")
+  .addEventListener("submit", getWeatherBySearch);
 document.querySelector("#btn").addEventListener("click", toggleState);
 
+// Get weather from submit form
+function getWeatherBySearch(e) {
+  e.preventDefault();
+  const city = document.getElementById("mainSearch").value;
+  api.changeLocation(city);
+  api
+    .getCurrentByID()
+    .then(res =>
+      res.cod === "400" || res.cod === "404"
+        ? console.log("City Not Found")
+        : console.log(res)
+    )
+    .catch(err => console.log(err));
+}
+
+// Load weather by current location. Set city to that location
+
+document.querySelector(".currLocation").addEventListener("click", () => {
+  api
+    .getByCoordinates()
+    .then(res => {
+      api.changeLocation(res.name);
+      console.log(res);
+    })
+    .catch(err => console.error(err));
+});
+
+// Load current weather by tabs
+document.getElementById("pills-home-tab").addEventListener("click", () => {
+  api
+    .getCurrentByID()
+    .then(res =>
+      res.cod === "400" || res.cod === "404"
+        ? console.log("City Not Found")
+        : console.log(res)
+    )
+    .catch(err => console.log(err));
+});
+
+// Load forecast weather by tabs
+document.getElementById("pills-profile-tab").addEventListener("click", () => {
+  api
+    .getForecastByCity()
+    .then(res =>
+      res.cod === "400" || res.cod === "404"
+        ? console.log("City Not Found")
+        : console.log(res)
+    )
+    .catch(err => console.log(err));
+});
+
+// Default weather data (dom loaded)
+function loadWeather() {
+  api
+    .getCurrentByID()
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
+}
+
+// Temperature (UNIT) button
 function toggleState(e) {
   const self = e.target;
   self.classList.toggle("metric");
@@ -23,9 +83,3 @@ function toggleState(e) {
     api.changeUnit("imperial");
   }
 }
-
-const loadWeather = () => {
-  api.getByCoordinates().then(data => console.log(data));
-};
-
-//loadWeather();
